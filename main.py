b51d7c2aef5c5986cb39662e794e7703ebd963d7
William Osbourn <william@osbourn.us>
2019-11-22 16:52:21 -0600

    added conditions in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template 
+from flask import Flask, request, redirect, render_template, escape
 import html
 import os
 
@@ -7,21 +7,39 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-  return render_template('signup.html') 
+  return render_template('home.html') 
 
 
-
-
-@app.route("/signup", methods= ['POST'])
+@app.route("/", methods= ['GET', 'POST'])
 def signup():
-  username = request.form['username']
-  password = request.form['password']
-  verify_password = request.form['verify_password']
-  email = request.form['emailaddress']
+  user_name = request.form['u_name']
+  password = request.form['p_word']
+  verify_password = request.form['verify']
+  e_mail = request.form['emailaddress']
 
-  if (not username) or (username.strip() == ""):
-    error = "That's not a valid username."
-    return redirect("/?error=" + error)  
+  username_error = ''
+  password_error = ''
+  v_password_error = ''
+
+  if (not user_name) or (user_name.strip() == "") or len(user_name) < 3 or len(user_name) > 20:
+    username_error = "That's not a valid username"
+    user_name = ''
+  if (not password) or (password.strip() == ""):
+    password_error = "That's not a valid password"
+    password = ''
+  if (not verify_password) or (verify_password.strip() == ""):
+    v_password_error = "That's not a valid password"
+    verify_password = ''
+  
+  
+  
+
+
+  
+  return render_template('home.html', u_error=username_error, p_error=password_error, username=user_name, p_word=password, v_pass_error=v_password_error, email=e_mail)
+
+
+    
 
 
 
