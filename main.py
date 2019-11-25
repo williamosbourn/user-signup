55d29f3fe0424a56d7248f735bc0d2e643d88ea3
William Osbourn <william@osbourn.us>
2019-11-25 16:30:48 -0600

    finished project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template, escape
 import html
 import os
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True 
@@ -20,6 +21,7 @@ def signup():
   username_error = ''
   password_error = ''
   v_password_error = ''
+  email_error = ''
 
   if (not user_name) or (user_name.strip() == "") or len(user_name) < 3 or len(user_name) > 20:
     username_error = "That's not a valid username"
@@ -30,18 +32,32 @@ def signup():
   if (not verify_password) or (verify_password.strip() == ""):
     v_password_error = "That's not a valid password"
     verify_password = ''
+  if verify_password != password:
+    v_password_error = "Passwords do not match"
+    verify_password = ''
+  
+  if len(e_mail) > 0:
+    if "@" not in e_mail or '.' not in e_mail or ' ' in e_mail:
+      email_error = "That's not a valid email"
+      e_mail = ''
+    else:
+      if len(e_mail) < 3 or len(e_mail) > 20:
+        email_error = "That's not a valid email"
+        e_mail = ''
+
   
   
+  if not username_error and not password_error and not v_password_error and not email_error:
+    username = request.form['u_name']
+    return render_template('welcome.html', username=username) 
   
+  return render_template('home.html', u_error=username_error, p_error=password_error, username=user_name, p_word=password, v_pass_error=v_password_error, email_error=email_error, email=e_mail) 
 
 
-  
-  return render_template('home.html', u_error=username_error, p_error=password_error, username=user_name, p_word=password, v_pass_error=v_password_error, email=e_mail)
-
-
-    
-
-
+@app.route("/welcome", methods=['POST'])
+def welcome():
+  username = request.form['u_name']
+  return render_template('welcome.html', username=username) 
 
 
 app.run()
